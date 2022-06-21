313a8b20ba5f9581f83d60126035941aaab7b82d
Chris Hokamp <chris.hokamp@gmail.com>
2022-06-21 18:15:22 +0100

    update wikimedia pageviews notebook

--- aylien_ts_datasets/yfinance_timeseries.py
+++ aylien_ts_datasets/yfinance_timeseries.py
@@ -23,12 +23,23 @@ def RSI(prices, interval=14):
     return RSI
 
 
-def retrieve_yfinance_timeseries(ticker, start_date):
-    raw_data = yfinance.download(ticker, start=str(start_date), threads=True)
-    raw_data =raw_data.rename(columns={'Adj Close': 'price'})
+def retrieve_yfinance_timeseries(tickers, start_date, end_date=None, interval='1d'):
+    if type(tickers) is list:
+        # format for yfinance bulk download
+        tickers = ' '.join(tickers)
+    if end_date is not None:
+        end_date = str(end_date)
+    raw_data = yfinance.download(
+        tickers, start=str(start_date), end=end_date, threads=True, interval=interval
+    )
+    raw_data = raw_data.rename(columns={'Adj Close': 'price'})
+
+    # Add columns with price transformations to RSI and derivative metrics
     raw_data['RSI'] = raw_data['price'].transform(lambda x: RSI(x))
     raw_data['RSI_quintile'] = raw_data['RSI'].transform(
         lambda group: pd.qcut(group, 5, labels=False, duplicates='drop'))
+
+    # TODO: is it correct to drop NA here?
     raw_data.dropna(inplace=True)
     num_days = 5
     for day in range(num_days + 1):
