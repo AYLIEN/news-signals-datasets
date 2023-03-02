7bbcfd4f7fa47c6db751a53115d10f67a379d224
Chris Hokamp <chris.hokamp@gmail.com>
2023-03-02 23:44:48 +0000

    better handling of anomaly detection edge cases

--- news_signals/anomaly_detection.py
+++ news_signals/anomaly_detection.py
@@ -15,9 +15,10 @@ class SigmaAnomalyDetector(AnomalyDetector):
     standard deviation.
     """
 
-    def __init__(self, sigma_multiple=1.):
+    def __init__(self, sigma_multiple=1., smoothing=1.):
         self.logger = create_logger(__name__)
         self.sigma_multiple = sigma_multiple
+        self.smoothing = smoothing
 
     def __call__(self, history, series, **kwargs):
         return self.anomalies_wrt_history(
@@ -30,17 +31,32 @@ class SigmaAnomalyDetector(AnomalyDetector):
 
     def anomalies_wrt_history(
             self,
-            history, test_series,
+            history, test_series, smoothing=None,
             sigma_multiple=None
     ):
+        try:
+            assert len(history) > 0, 'history must be non-empty'
+        except AssertionError as e:
+            raise e
+        if sigma_multiple is None:
+            sigma_multiple = self.sigma_multiple
+        if smoothing is None:
+            smoothing = self.smoothing
+        history = history + smoothing
+        test_series = test_series + smoothing
         if sigma_multiple is None:
             sigma_multiple = self.sigma_multiple
         sigma = self.sigma(history)
+        # ensures that sigma shouldn't be zero
+        # user can override this by setting smoothing to zero,
+        # that could result in infinite anomaly score
+        sigma = max(sigma, smoothing) 
         mean = history.mean()
         test_series = test_series - mean
         # how many standard deviations away does it need
         # to be outside to be an anomaly?
         multiple = sigma * sigma_multiple
+
         test_series = test_series / multiple
         # note this positive-only modification makes sense for
         # news, but not for other series types like stocks
---
+++ news_signals/test_anomaly_detection.py
@@ -0,0 +1,56 @@
+import os
+import unittest
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+from news_signals.anomaly_detection import SigmaAnomalyDetector
+
+
+path_to_file = Path(os.path.dirname(os.path.abspath(__file__)))
+resources = Path(os.environ.get(
+    'RESOURCES', path_to_file / '../resources/test'))
+
+
+class TestSigmaAnomalyDetector(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def test_sigma_anomaly_detector(self):
+        anomaly_detector = SigmaAnomalyDetector(sigma_multiple=1.)
+        timeseries = pd.Series([10, 20, 30, 40, 0])
+        history = timeseries[:-1]
+        test_series = timeseries[-1:]
+        anomaly_series = anomaly_detector(history, test_series)
+        assert anomaly_series.iloc[0] == 0.0
+
+        timeseries = pd.Series([0, 0, 0, 0, 100])
+        history = timeseries[:-1]
+        test_series = timeseries[-1:]
+        anomaly_series = anomaly_detector(history, test_series)
+        assert anomaly_series.iloc[0] == 100
+
+        timeseries = pd.Series([0])
+        history = timeseries[:-1]
+        test_series = timeseries[-1:]
+        with self.assertRaises(AssertionError):
+            anomaly_series = anomaly_detector(history, test_series)
+        
+        timeseries = pd.Series([20, 30, 20, 30, 50])
+        history = timeseries[:-1]
+        test_series = timeseries[-1:]
+        anomaly_series = anomaly_detector(history, test_series)
+        assert np.isclose(anomaly_series.iloc[0], 4.3, atol=0.1)
+
+
+if __name__ == '__main__':
+    unittest.main()
